417f03535a743d138dd6f39bf245af080038bd3c
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-11 17:55:24 +0530

    app container and input created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Addicon from './plus.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <h2 className='app-tital'>To Do Smart</h2>
+      <div className="list-group">
+
+      </div>
+      <div className='input-container'>
+        <input type="text" className="input-todo" />
+        <img src={Addicon} alt="" className="add-icon" />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
