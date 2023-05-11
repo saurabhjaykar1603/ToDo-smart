bc218dd3d672e08968c5caa4bf416b6c07bfacb8
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-11 18:16:53 +0530

    using usestate map func with array created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Addicon from './plus.png'
 
 function App() {
+  const [mylist, setMyList,] = useState([]);
+
   return (
     <div className='app-container'>
       <h2 className='app-tital'>To Do Smart</h2>
-      <div className="list-group">
 
+      <div className="list-group">
+        {
+          mylist.map((task) => {
+            return (
+              <div className="task">
+                {task}
+              </div>
+            )
+          })
+        }
       </div>
+
       <div className='input-container'>
         <input type="text" className="input-todo" />
         <img src={Addicon} alt="" className="add-icon" />
