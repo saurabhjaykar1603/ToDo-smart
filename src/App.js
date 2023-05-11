97cb0061b27bcec33979d96d3cce482802dc935d
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-11 18:44:47 +0530

    task empty using usestate settask

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
     // The JavaScript spread operator ( ... ) allows us to quickly copy all or part of an existing array or object into another array or object.
 
     setMyList([...mylist, task]);
+    setTask('')
   }
 
   return (
